80fef68680bae2c63628117ba0cdf2d58d78c576
Elan Ruusamäe <glen@delfi.ee>
2013-01-18 17:43:21 +0200

    simplify, make more generic

--- create_repo.sh
+++ create_repo.sh
@@ -1,12 +1,12 @@
+#!/bin/sh
+
 # Create repo
-mkdir eventum
+install -d eventum
 cd eventum
-git init 
+git init
 
 # Prepare for bzr
-mkdir .git/bzr
-mkdir .git/bzr/repo
-mkdir .git/bzr/map
+install -d .git/bzr/{map,repo}
 bzr init-repo --no-trees .git/bzr/repo
 bzr branch lp:eventum .git/bzr/repo/master
 
@@ -14,18 +14,17 @@ bzr branch lp:eventum .git/bzr/repo/master
 bzr fast-export --plain --export-marks=.git/bzr/map/master-bzr --git-branch=bzr/master .git/bzr/repo/master > bzr-fast-export
 
 # Fixing the repo committers
-sed "s/^committer <Elan>/committer Elan Ruusamäe <glen@delfi.ee>/" bzr-fast-export > bzr-fast-export-fix
-sed "s/^committer <>/committer Unknown <noreply@launchpad.net>/" bzr-fast-export-fix > bzr-fast-export
-rm bzr-fast-export-fix
+sed -i -e "s/^committer <Elan>/committer Elan Ruusamäe <glen@delfi.ee>/" bzr-fast-export
+sed -i -e "s/^committer <>/committer Unknown <noreply@launchpad.net>/" bzr-fast-export
 
 # Import into git
 git fast-import --quiet --export-marks=.git/bzr/map/master-git < bzr-fast-export
-rm bzr-fast-export
+rm -f bzr-fast-export
 git branch master bzr/master
 git config bzr.master.bzr bzr/master
 git config bzr.bzr/master.upstream lp:eventum
 git checkout master
 
 git bzr sync
-git remote add github git@github.com:petterl/eventum.git
-git push github master
+#git remote add github git@github.com:petterl/eventum.git
+#git push github master
--- sync-repos.sh
+++ sync-repos.sh
@@ -1,9 +1,11 @@
-REPOBASE=$HOME/repos
+#!/bin/sh
+REPOBASE=$(dirname "$0")
+REPOBASE=$(cd "$REPOBASE"; pwd)
 
 for REPO in eventum; do
-  cd $REPOBASE/$REPO/
-  echo "Syncing $REPO repository from launchpad.."
-  git bzr pull
-  echo "Pushing $REPO repository to github.."
-  git push github master
+	cd $REPOBASE/$REPO/
+	echo "Syncing $REPO repository from launchpad.."
+	git bzr pull
+	echo "Pushing $REPO repository to github.."
+	git push github master
 done
