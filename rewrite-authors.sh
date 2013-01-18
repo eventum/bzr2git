cace5b2c1c408baf992f1d4a0945d34ad5d55eb1
Elan Ruusamäe <glen@delfi.ee>
2013-01-18 21:56:11 +0200

    add bryan and jpm

--- rewrite-authors.sh
+++ rewrite-authors.sh
@@ -9,11 +9,38 @@ git filter-branch -f --env-filter '
 	case "$GIT_AUTHOR_EMAIL:$GIT_AUTHOR_NAME" in
 	*:"=?utf-8?q?Elan_Ruusam=C3=A4e?=" | \
 	*:"Launchpad Translations on behalf of glen666" | \
+	codehost@crowberry:"Launchpad Code Hosting" | \
 	:glen | \
 	glen@delfi.ee:glen | \
 	_____ )
 		export GIT_AUTHOR_NAME="Elan Ruusamäe"
 		export GIT_AUTHOR_EMAIL="glen@delfi.ee"
+		export GIT_COMMITTER_NAME=$GIT_AUTHOR_NAME
+		export GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL
+		;;
+	esac
+
+	case "$GIT_AUTHOR_EMAIL:$GIT_AUTHOR_NAME" in
+	:"mysql.com!bryan" | \
+	:"balsdorf" | \
+	:"jesushatesjava.com!bryan/balsdorf" | \
+	:"jesushatesjava.com!balsdorf" | \
+	:"!balsdorf" | \
+	:"!bryan" | \
+	bryan@achilles:"Bryan Alsdorf" | \
+	bryan@montyprogram.com:"Bryan Alsdorf" | \
+	_____ )
+		export GIT_AUTHOR_NAME="Bryan Alsdorf"
+		export GIT_AUTHOR_EMAIL="bryan@askmonty.org"
+		export GIT_COMMITTER_NAME=$GIT_AUTHOR_NAME
+		export GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL
+		;;
+	esac
+
+	case "$GIT_AUTHOR_EMAIL:$GIT_AUTHOR_NAME" in
+	:"mysql.com!jpm")
+		export GIT_AUTHOR_NAME="João Prado Maia"
+		export GIT_AUTHOR_EMAIL="jpm@mysql.com"
 		export GIT_COMMITTER_NAME=$GIT_AUTHOR_NAME
 		export GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL
 		;;
