b1086988e6142cfb3f32be941da3e1c99f0fa172
Elan Ruusamäe <glen@delfi.ee>
2013-01-19 12:49:07 +0200

    use local bzr repo for migration

--- create_repo.sh
+++ create_repo.sh
@@ -1,25 +1,39 @@
 #!/bin/sh
+set -e
+
+dir=$(dirname "$0")
+dir=$(cd "$dir"; pwd)
+
+# checkout bazaar locally, to cut down network traffic if want to restart process
+test -d eventum.bzr || bzr branch lp:eventum eventum.bzr
+
+# live one
+#bzr_upstream=lp:eventum
+# test one
+bzr_upstream=$dir/eventum.bzr
 
 # Create repo
-install -d eventum
-cd eventum
+# mkdir, so it will fail if dir exists
+mkdir bzr2git
+cd bzr2git
 git init
 
 # Prepare for bzr
 install -d .git/bzr/{map,repo}
 bzr init-repo --no-trees .git/bzr/repo
-bzr branch lp:eventum .git/bzr/repo/master
+bzr branch $bzr_upstream .git/bzr/repo/master
 
 # Fast-export the bzr repo
+#--authors-file=authors
 bzr fast-export --plain --export-marks=.git/bzr/map/master-bzr --git-branch=bzr/master .git/bzr/repo/master > bzr-fast-export
 
 # Import into git
 git fast-import --quiet --export-marks=.git/bzr/map/master-git < bzr-fast-export
-rm -f bzr-fast-export
+#rm -f bzr-fast-export
 git branch master bzr/master
 git config bzr.master.bzr bzr/master
-git config bzr.bzr/master.upstream lp:eventum
-git checkout master
+git config bzr.bzr/master.upstream $bzr_upstream
+#git checkout master
 
 git bzr sync
 #git remote add github git@github.com:petterl/eventum.git
