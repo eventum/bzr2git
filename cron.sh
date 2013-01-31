d117f1e7d7a2133b62c790b3f778bfec1b5d1d4f
Elan Ruusamäe <glen@delfi.ee>
2013-01-31 23:44:36 +0200

    script used to run syncing from cron.
    depends on github WebHook Service Hook putting lockfile up which
    existence means synchro needed from github side

---
+++ cron.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+PROGRAM=eventum-sync
+dir=$(dirname "$0")
+cd "$dir"
+
+lock=~/www/spool/eventum-sync.json
+
+test -e $lock || exit 0
+
+(
+	flock -n 9 || { echo "$PROGRAM: locked"; exit 1; }
+
+	# load deploy key to ssh agent that bzr/git can use
+	eval `ssh-agent` >/dev/null
+	ssh-add id_eventum 2>/dev/null
+	trap "set -x; kill $SSH_AGENT_PID" 0
+
+	sh -x sync-repos.sh
+
+	rm $lock
+) 9<$lock
